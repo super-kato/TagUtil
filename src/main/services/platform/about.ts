4dffb00b3a350c4eaef81711e58a0441497f34c1
super-kato <mikewazowskiandmary@gmail.com>
2026-04-12 03:40:58 +0000

    fix: aboutページ

--- src/main/services/platform/about.ts
+++ src/main/services/platform/about.ts
@@ -2,6 +2,7 @@ import openAboutWindow from 'about-window';
 import { app } from 'electron';
 import { existsSync, readFileSync } from 'fs';
 import { join } from 'path';
+import pkg from '../../../../package.json';
 import icon from '../../../../resources/icon.png?asset';
 
 /**
@@ -22,12 +23,16 @@ export const showAboutWindow = (): void => {
   openAboutWindow({
     /* eslint-disable @typescript-eslint/naming-convention */
     icon_path: icon,
-    package_json_dir: app.getAppPath(),
     product_name: 'TagUtil',
-    copyright: 'Copyright (c) 2026 y.kato',
+    copyright: 'Copyright (c) 2026 katouyoshiaki',
     homepage: 'https://github.com/super-kato/TagUtil',
     bug_report_url: 'https://github.com/super-kato/TagUtil/issues',
-    use_version_info: true,
+    use_version_info: [
+      ['Version', pkg.version],
+      ['Electron', process.versions.electron],
+      ['Node', process.versions.node],
+      ['Chrome', process.versions.chrome]
+    ],
     // @ts-ignore: additional_credits exists in about-window
     additional_credits: credits
     /* eslint-enable @typescript-eslint/naming-convention */
